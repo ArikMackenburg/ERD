722edf1242d6bee9c5cdd3220993c7828b64d7c5
ArikMackenburg <Arikmackenburg@gmail.com>
2020-09-09 12:05:11 -0500

    EF Startup configuration

--- Web/Startup.cs
+++ Web/Startup.cs
@@ -5,17 +5,37 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Web.Data;
 
 namespace Web
 {
     public class Startup
     {
+        //1.
+        public IConfiguration Configuration { get; }
+
+        //2. Add constructor to recieve configuration
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            //3. Register our DbContext with app
+            services.AddDbContext<HotelDbContext>(options =>
+            {
+                // Equivalent to DATABASE_URL
+                string connectionString = Configuration.GetConnectionString("DefaultConnection");
+                options.UseSqlServer(connectionString);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -33,6 +53,10 @@ namespace Web
                 endpoints.MapGet("/", async context =>
                 {
                     await context.Response.WriteAsync("Hello World!");
+                });
+                endpoints.MapGet("/boom", async context =>
+                {
+                    throw new Exception();
                 });
             });
         }

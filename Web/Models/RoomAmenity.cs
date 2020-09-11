812eabf01696fe15066ffd24b06af18bcfaaf177
ArikMackenburg <Arikmackenburg@gmail.com>
2020-09-11 17:57:34 -0500

    reseted and added packages

---
+++ Web/Models/RoomAmenity.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web.Models
+{
+    public class RoomAmenity
+    {
+        
+        public int RoomId { get; set; }
+        
+        public int AmenityId { get; set; }
+        
+        public Room Room { get; set; }
+        
+        public Amenity Amenity { get; set; }
+    }
+}
